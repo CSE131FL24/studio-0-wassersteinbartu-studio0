5eb03920c12a611c4bc2aec850f104775fcaf6b4
leahwasserstein <leahwasserstein@localhost>
2024-08-28 13:52:35 -0500

    Finsihed up to message

--- src/studio0/Message.java
+++ src/studio0/Message.java
@@ -3,7 +3,7 @@ package studio0;
 public class Message {
 
 	public static void main(String args[]) {
-		System.out.println("HI");
+		System.out.println("We are in computer science class!");
 		
 		
 	}
